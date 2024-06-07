327f4d54cd90937d959347e838c67213dd7c3429
Rodrigo Ramos <rodrigoramosrs2@hotmail.com>
2024-06-07 19:42:29 -0300

    Fix some invalid path

--- src/Utils/PathUtils.cs
+++ src/Utils/PathUtils.cs
@@ -8,18 +8,22 @@ namespace jsunmap.Utils
 {
     internal static class PathUtils
     {
+        private static char[] AllInvalidChars = Path.GetInvalidFileNameChars().Concat(Path.GetInvalidPathChars()).ToArray();
+        private static char[] PathInvalidChars = Path.GetInvalidPathChars();
+        private static char[] FileInvalidChars = Path.GetInvalidFileNameChars();
         public static string RemoveInvalidFilenameCharacters(string input)
         {
-            return InternalRemoveInvalidChar(Path.GetInvalidFileNameChars(),input);
+            return InternalRemoveInvalidChar(FileInvalidChars, input);
         }
-        
+
         public static string RemoveInvalidPathCharacters(string input)
         {
-            return InternalRemoveInvalidChar(Path.GetInvalidPathChars() ,input);
+            return InternalRemoveInvalidChar(PathInvalidChars, input)
+                ;
         }
         public static string RemoveAllInvalidCharacters(string input)
         {
-            return InternalRemoveInvalidChar(Path.GetInvalidFileNameChars().Concat(Path.GetInvalidPathChars()).ToArray(), input);
+            return InternalRemoveInvalidChar(AllInvalidChars, input);
         }
         private static string InternalRemoveInvalidChar(char[] invalidChars, string input)
         {
@@ -31,7 +35,11 @@ namespace jsunmap.Utils
                     output.Append(c);
                 }
             }
-            return output.ToString();
+            return output.ToString()
+                .Replace("%20", "_")
+                .Replace("^", string.Empty)
+                .Replace("$", string.Empty)
+                .Replace("*", string.Empty) ;
         }
     }
 }
